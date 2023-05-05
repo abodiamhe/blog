93317696c01f112c0400ea7449551940d6e6e9f8
abodiamhe <akhigbesadiq@gmail.com>
2023-05-05 16:54:01 +0100

    initial commit

--- app.js
+++ app.js
@@ -77,6 +77,11 @@ Post.findOne({_id: requestedPostId}).then(function(post){
 })
 });
 
-app.listen(3000, function() {
+
+let port = process.env.PORT;
+if (port == null || port = ""){
+  port = 3000;
+}
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
